14f4ef9c1072cc8987b579b0cb5b23ef3d47414c
lisdelavallee <lisrutanga@gmail.com>
2025-04-12 19:42:59 +0200

    site.php
    introduction

--- site.php
+++ site.php
@@ -1,8 +1,14 @@
 <!doctype html>
     <html>
         <head>
-            <title>our first php<title>
+            <title>our first php<title> 
+            </head>
+                <body>X
                 <?php
                 echo "we have learnt alot today/n";
                 echo "such as html/n";
                 echo "and also php programming/n";
+?>
+</body>
+</html
+    
