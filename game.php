f3e63d025dad67ccdf9c4b486c9676ebecfa767b
lisdelavallee <lisrutanga@gmail.com>
2025-04-12 20:05:48 +0200

    game.php
    introduction

---
+++ game.php
@@ -0,0 +1,16 @@
+<!doctype html>
+    <html>
+        <head>
+            <title>fun game</title>
+            <body>
+                
+                <?php
+          
+          $char1 = "umwiza cyntia";
+          $char2 = "lis de la vallee niyigena";
+          $char3 = "remy";
+          
+          echo "$char1, $char2, and $char3 have created php";
+          ?>
+          </body>
+          </html>
